05241a1dc70ea246e3e9c1bd6bc32f95309f3bf4
UtilityAcc <96780372+UtilityAcc@users.noreply.github.com>
2022-06-20 08:53:27 +0300

    Update User.java
    Construct

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -24,11 +24,13 @@ public class User {
 
    public User() {}
    
-   public User(String firstName, String lastName, String email) {
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
+
 
    public Long getId() {
       return id;
@@ -64,9 +66,5 @@ public class User {
 
    public Car getCar() {
       return car;
-   }
-
-   public void setCar(Car car) {
-      this.car = car;
    }
 }
